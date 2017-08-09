fdd84b518eceee31da8acc9160127f6c8f9251cd
Vrungel <nikita241296@gmail.com>
2017-08-09 14:45:39 +0300

    DrawerActivity add

---
+++ app/src/main/java/floor/twelve/apps/com/medical/feature/start_point/activities/MainActivity322.java
@@ -0,0 +1,13 @@
+package floor.twelve.apps.com.medical.feature.start_point.activities;
+
+import android.os.Bundle;
+import floor.twelve.apps.com.medical.R;
+import floor.twelve.apps.com.medical.feature.start_point.DrawerActivity;
+
+public class MainActivity322 extends DrawerActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    setContentView(R.layout.activity_main322);
+    super.onCreate(savedInstanceState);
+  }
+}
